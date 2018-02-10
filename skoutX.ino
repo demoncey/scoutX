8295332ff9668c4b5f452e64fa8a888982e4ff73
demoncey <demoncey@gmail.com>
2018-02-10 15:24:31 +0100

    serial added

--- skoutX.ino
+++ skoutX.ino
@@ -1,13 +1,37 @@
 #include <supervisor.h>
 #include <task.h>
+void send_msg();
+
+
+
+Supervisor supervisor("scoutX supervisor");
+Task sMsgTask(&send_msg);
+
+
+
+
 
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+    while (!Serial) {
+    ; 
+  }
+  supervisor.addTask(sMsgTask);
+  sMsgTask.setPriority(P_HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  supervisor.execute();
+  delay(1000);
 
+}
+
+
+
+
+void send_msg() {
+  int id = random(1, 1000000000);
+  Serial.println("Heartbeat msg: "+String(id));
 }
